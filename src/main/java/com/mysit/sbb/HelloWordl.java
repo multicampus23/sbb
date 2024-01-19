c9f96d7ccd3a3c971d9b2862a2c483815ffb8bb9
Jangwoo Park <p.jangwoo@gmail>
2024-01-19 11:12:30 +0900

    1월 19일 - 아침

--- src/main/java/com/mysit/sbb/HelloWordl.java
+++ src/main/java/com/mysit/sbb/HelloWordl.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWordl {
 	
+	String a; 
+	
 	//http://localhost:8585/hello 
 	@GetMapping("/hello")
 	@ResponseBody
