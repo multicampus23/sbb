540d18af97d72e76dd020d7c1396f546d36b583e
Jangwoo Park <p.jangwoo@gmail>
2024-01-25 15:18:46 +0900

    1월 25일 - 15시

--- src/main/java/com/mysit/sbb/SecurityConfig.java
+++ src/main/java/com/mysit/sbb/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -21,9 +22,20 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
 		http
+			//http://localhost:8585/	: / 요청은 인증없이 접근 허용 
 			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
 					.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-					; 
+		
+			//http://localhost:8585/h2-console  : csrf 보안키 없이도 접근을 허용  
+			.csrf( (csrf) -> csrf
+					.ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))				
+					)
+			// 웹페이지의 프레임을 사용할 수 있도록 허용 
+			.headers( (headers) -> headers
+					.addHeaderWriter(new XFrameOptionsHeaderWriter(
+							XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))					
+					)
+		; 
 							
 		return http.build(); 
 	}
