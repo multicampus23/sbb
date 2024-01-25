39ff91c5ec83493c6b801d3a55d833b9a8f97ea9
Jangwoo Park <p.jangwoo@gmail>
2024-01-25 16:20:49 +0900

    1월 25일 - 16시

--- src/main/java/com/mysit/sbb/SecurityConfig.java
+++ src/main/java/com/mysit/sbb/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -39,6 +41,13 @@ public class SecurityConfig {
 							
 		return http.build(); 
 	}
+	
+	// 암호화 객체 생성후 Spring 프레임워크에 객체를 등록 함. 
+	@Bean
+	PasswordEncoder passwordEncoder() {	
+		return new BCryptPasswordEncoder(); 
+	}
+	
 	
 	
 }
