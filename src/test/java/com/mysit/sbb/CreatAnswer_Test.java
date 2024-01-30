edfcb750063fc4515d26873a8fa540237655d9d4
Jangwoo Park <p.jangwoo@gmail>
2024-01-30 10:24:52 +0900

    1월 30일 10시

---
+++ src/test/java/com/mysit/sbb/CreatAnswer_Test.java
@@ -0,0 +1,38 @@
+package com.mysit.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysit.sbb.answer.AnswerService;
+import com.mysit.sbb.user.SiteUser;
+import com.mysit.sbb.user.UserService;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+public class CreatAnswer_Test {
+	
+	@Autowired
+	private AnswerService answerService; 
+	private UserService userService ; 
+	
+	
+	// AnswerService의 createAnswer(Integer id , String content, SiteUser author) 
+	// answer 테이블의 값이 잘 들어가는지 테스트 
+	@Transactional
+	@Test
+	public void createAnswerTest() {
+		
+		// 첫 번째 인자 : id  : question 테이블의 id 
+		// 두 번째 인자 : content  : 답변 내용 
+		// 세 번째 인자 : author : SiteUser 객체가 
+		SiteUser author = 
+				this.userService.getUser("user1"); 
+		
+		answerService.creatAnswer(1852	, "1852 질문에 대한 답변 입니다. ", author);
+		
+		
+	}
+
+}
