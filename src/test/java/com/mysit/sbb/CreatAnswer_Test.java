09e1949fc1ccacce0115285b38a12c6025ea60b8
Jangwoo Park <p.jangwoo@gmail>
2024-01-30 10:31:42 +0900

    1월 30일 - 10시 30분

--- src/test/java/com/mysit/sbb/CreatAnswer_Test.java
+++ src/test/java/com/mysit/sbb/CreatAnswer_Test.java
@@ -1,11 +1,14 @@
 package com.mysit.sbb;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mysit.sbb.answer.AnswerService;
 import com.mysit.sbb.user.SiteUser;
+import com.mysit.sbb.user.UserRepository;
 import com.mysit.sbb.user.UserService;
 
 import jakarta.transaction.Transactional;
@@ -16,6 +19,8 @@ public class CreatAnswer_Test {
 	@Autowired
 	private AnswerService answerService; 
 	private UserService userService ; 
+	private UserRepository userRepository; 
+	
 	
 	
 	// AnswerService의 createAnswer(Integer id , String content, SiteUser author) 
@@ -27,8 +32,14 @@ public class CreatAnswer_Test {
 		// 첫 번째 인자 : id  : question 테이블의 id 
 		// 두 번째 인자 : content  : 답변 내용 
 		// 세 번째 인자 : author : SiteUser 객체가 
-		SiteUser author = 
-				this.userService.getUser("user1"); 
+//		SiteUser author = 
+//				this.userService.getUser("user1"); 
+		
+		Optional<SiteUser> _author = 
+				userRepository.findByusername("user1"); 
+		
+		SiteUser author = _author.get(); 
+		
 		
 		answerService.creatAnswer(1852	, "1852 질문에 대한 답변 입니다. ", author);
 		
