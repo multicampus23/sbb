1910ec1b5d6ec142275fa0c0143a9c8b29bc24bc
Jangwoo Park <p.jangwoo@gmail>
2024-01-31 16:26:37 +0900

    1월 31일 - 16시

--- src/test/java/com/mysit/sbb/question/Question_Select_Test2.java
+++ src/test/java/com/mysit/sbb/question/Question_Select_Test2.java
@@ -1,5 +1,6 @@
 package com.mysit.sbb.question;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,33 @@ public class Question_Select_Test2 {
 			System.out.println(list.get(i).getContent());
 			System.out.println();		
 		}
+		
+		System.out.println("====향상된 FOR 문 : 방번호가 없는 경우 :  Set, Map, Page===================");
+		
+		for (Question q : list) {
+			System.out.println(q.getId());
+			System.out.println(q.getSubject());
+			System.out.println(q.getContent());
+			System.out.println();
+		}
+		
+		System.out.println("===Iterator 출력  : 순회자 , Set, Map, Page==============");
+		
+		// Iterator 선언 : 
+		Iterator<Question> iterator = list.iterator() ; 
+		
+		// while 문으로 출력 : iterator.hasNext() <== 다음의 값이 존재하면 true 
+		//					 iterator.next() <== 현재 위치의 객체를 끄집어 내고 다음 위치로 이동 
+		while (iterator.hasNext()) {
+			
+			Question q = iterator.next();
+			
+			System.out.println(q.getId());
+			System.out.println(q.getSubject());
+			System.out.println(q.getContent());
+			System.out.println();
+		}
+		
 			
 	}
 }
--- src/test/java/com/mysit/sbb/question/Question_Select_Test3.java
+++ src/test/java/com/mysit/sbb/question/Question_Select_Test3.java
@@ -1,6 +1,7 @@
 package com.mysit.sbb.question;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -24,7 +25,7 @@ public class Question_Select_Test3 {
 		sorts.add(Sort.Order.desc("createDate")); 
 		
 		// Pageabl 객체에 : 요청하는페이지번호, 한페이지에 출력될 레코드 갯수, 정렬 
-		Pageable pageable = PageRequest.of(103, 10, Sort.by(sorts)); 
+		Pageable pageable = PageRequest.of(0, 10, Sort.by(sorts)); 
 		
 		Page<Question> pages = 
 				questionRepository.findAll(pageable); 
@@ -36,7 +37,30 @@ public class Question_Select_Test3 {
 		System.out.println("현재 요청 페이지 번호 : " + pages.getNumber());
 		System.out.println("이전페이지 존재여부 : " + pages.hasPrevious());
 		System.out.println("다음 페이지 존재여부 : " + pages.hasNext());
+		System.out.println("===================================");	
+		
+		// Page 는 방번호가 없음 : 향상된 FOR, Iterator 를 사용해서 출력 
+		
+		System.out.println("==== 향상된 FOR 문으로 출력 =======");
+		for (Question q : pages) {
+			System.out.println(q.getId());
+			System.out.println(q.getSubject());
+			System.out.println(q.getContent());
+			System.out.println();
+		}
+		
+		System.out.println("==== Iterator 으로 출력 =======");
+		// Iterator 선언(장착) 
+		Iterator<Question> iterator = pages.iterator(); 
+		
+		while (iterator.hasNext()) {
 			
+			Question q = iterator.next(); 
+			System.out.println(q.getId());
+			System.out.println(q.getSubject());
+			System.out.println(q.getContent());
+			System.out.println();
+		}
 		
 	}
 	
